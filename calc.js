0984ac1a45eac4c6510573a8d332cedf9469be23
ridermansb <ridermansb@gmail.com>
2017-06-03 12:04:26 +1200

    Add sub feature

--- calc.js
+++ calc.js
@@ -1,3 +1,4 @@
 module.exports = {
-  sum: (n1, n2) => n1 + n2
+  sum: (n1, n2) => n1 + n2,
+  sub: (n1, n2) => n1 - n2
 }
--- calc.test.js
+++ calc.test.js
@@ -2,3 +2,5 @@ const assert = require('assert');
 const calc = require('./calc');
 
 assert.equal(calc.sum(1,1), 2);
+
+assert.equal(calc.sub(10,5), 5);
